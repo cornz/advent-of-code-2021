b7affdbef5fdac9cce0f66d0e7405fb1583c1bd3
cornz <cornz@users.noreply.github.com>
2021-12-01 21:22:12 +0100

    solve part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -11,11 +11,22 @@ fun main() {
         return i
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var i = 0;
+        var previousMeasureSum: Int = Int.MAX_VALUE;
+        val summedWindow = input.windowed(size = 3, step = 1, partialWindows = true) { window -> window.sum() }
+        for (window: Int in summedWindow) {
+            if (window > previousMeasureSum) {
+                i += 1
+            }
+            previousMeasureSum = window
+        }
+        return i
     }
 
     val input = readInput("Day01")
-    println(part1(input.map { it.toInt()}))
+    val input_test = readInput("Day01")
+    // println(part1(input.map { it.toInt()}))
+    println(part2(input_test.map { it.toInt()}))
 
 }
