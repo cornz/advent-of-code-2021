26aa9270827a8d6ce03026df0976d0f14ce90ad3
cornz <cornz@users.noreply.github.com>
2021-12-02 11:44:32 +0100

    solve day2 part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,11 +3,11 @@ fun main() {
         var depth = 0
         var horizontal = 0
         for (row: String in input) {
-            val action: String = row.split(" ")[0]
-            val value: Int = row.split(" ")[1].toInt()
+            val action = row.split(" ")[0]
+            val value = row.split(" ")[1].toInt()
             when (action) {
                 "forward" -> {
-                    horizontal += value;
+                    horizontal += value
                 }
                 "down" -> {
                     depth += value
@@ -20,14 +20,32 @@ fun main() {
         return depth*horizontal
     }
 
-    fun part2(input: List<Int>): Int {
-
-        return i
+    fun part2(input: List<String>): Int {
+        var depth = 0
+        var horizontal = 0
+        var aim = 0
+        for (row: String in input) {
+            val action = row.split(" ")[0]
+            val value = row.split(" ")[1].toInt()
+            when (action) {
+                "forward" -> {
+                    horizontal += value
+                    depth += aim * value
+                }
+                "down" -> {
+                    aim += value
+                }
+                "up" -> {
+                    aim -= value
+                }
+            }
+        }
+        return depth*horizontal
     }
 
-    val input = readInput("Day02_Test")
-    val input_test = readInput("Day02")
-    // println(part1(input.map { it.toInt()}))
-    println(part1(input_test))
+    val inputTest = readInput("Day02_test")
+    val input = readInput("Day02")
+    println(part2(inputTest))
+    println(part2(input))
 
 }
