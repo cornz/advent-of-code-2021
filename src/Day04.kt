25e9a6c8d764354fef7a76cbd10066cd1bef1abe
cornz <cornz@users.noreply.github.com>
2021-12-04 11:31:43 +0100

    start day04 part1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,7 +1,45 @@
 fun main() {
 
+    fun getWinningRows(input: List<String>): List<List<Int>> {
+        val winningRows = mutableListOf<List<Int>>()
+        val sizeOfMatrix = input[2].replace("  ", " ").trim().split(" ").map { it.toInt() }.count()
+
+        for (i in 2..6) {
+            val row = input[i].replace("  ", " ").trim().split(" ").map { it.toInt() }
+            winningRows.add(row)
+        }
+        val winningRowsVertical = mutableListOf<List<Int>>()
+        for (n in 0 until sizeOfMatrix) {
+            val row = mutableListOf<Int>()
+            for (i in 0 until sizeOfMatrix) {
+                row.add(winningRows[i][n])
+            }
+            winningRowsVertical.add(row)
+        }
+        val crossRowTopBottom = mutableListOf<Int>()
+        for (i in 0 until sizeOfMatrix) {
+            crossRowTopBottom.add(winningRows[i][i])
+        }
+        val crossRowBottomTop = mutableListOf<Int>()
+        var backOrder = sizeOfMatrix - 1
+        for (i in 0 until sizeOfMatrix) {
+            crossRowBottomTop.add(winningRows[i][backOrder])
+            backOrder -= 1
+        }
+        val winningRowsTotal = mutableListOf<List<Int>>()
+        winningRowsTotal.addAll(winningRows)
+        winningRowsTotal.addAll(winningRowsVertical)
+        winningRowsTotal.add(crossRowTopBottom)
+        winningRowsTotal.add(crossRowBottomTop)
+
+        return winningRowsTotal
+    }
+
     fun part1(input: List<String>): Int {
-        return 0
+        val drawnNumbers = input[0].split(",").map { it.toInt() }
+        val winningRows = getWinningRows(input)
+
+        return winningRows.count()
     }
 
     fun part2(input: List<String>): Int {
@@ -15,5 +53,11 @@ fun main() {
     println(part1(input))
     println(part2(inputTest))
     println(part2(input))
+
+    class BingoCard(
+        val cardValue: Int,
+        val isChecked: Boolean
+    )
+
 
 }
