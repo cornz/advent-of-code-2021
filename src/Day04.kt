4ebc20a9c8a0c66ffe920fb107314015f5a05681
Cornelius Putzler-Marci <cornz@users.noreply.github.com>
2021-12-04 09:46:47 +0100

    add day4 template

---
+++ src/Day04.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+
+    val inputTest = readInput("Day04_test")
+    val input = readInput("Day04")
+    println(part1(inputTest))
+    println(part1(input))
+    println(part2(inputTest))
+    println(part2(input))
+
+}
