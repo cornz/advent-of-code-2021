a3454caab72de00ba83b77a244397a04e9f92c4c
cornz <cornz@users.noreply.github.com>
2021-12-05 22:50:21 +0100

    solve part2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,3 +1,7 @@
+import java.lang.Math.abs
+import java.lang.Math.max
+import kotlin.math.roundToInt
+
 fun main() {
 
     fun countOccurrencesOverOne(grid: Array<Array<Int>>): Int {
@@ -41,8 +45,6 @@ fun main() {
             val fromY = fromTo[0].split(",")[1].toInt()
             val toX = fromTo[1].split(",")[0].toInt()
             val toY = fromTo[1].split(",")[1].toInt()
-            //grid[fromY][fromX] = grid[fromY][fromX]+1
-            //grid[toY][toX] = grid[toY][toX]+1
             if (fromX == toX) {
                 if (fromY > toY) {
                     for (i in toY .. fromY) {
@@ -71,7 +73,52 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val maxNumber = findMaxNumberInInput(input)
+        val grid = Array(maxNumber) {
+            Array(maxNumber) {
+                0
+            }
+        }
+        for (row: String in input) {
+            val fromTo = row.split(" -> ")
+            val fromX = fromTo[0].split(",")[0].toInt()
+            val fromY = fromTo[0].split(",")[1].toInt()
+            val toX = fromTo[1].split(",")[0].toInt()
+            val toY = fromTo[1].split(",")[1].toInt()
+            if (fromX == toX) {
+                if (fromY > toY) {
+                    for (i in toY .. fromY) {
+                        grid[i][fromX] += 1
+                    }
+                } else {
+                    for (i in fromY .. toY) {
+                        grid[i][fromX] += 1
+                    }
+                }
+            } else if (fromY == toY) {
+                if (fromX > toX) {
+                    for (i in toX .. fromX) {
+                        grid[fromY][i] += 1
+                    }
+                } else {
+                    for (i in fromX..toX) {
+                        grid[fromY][i] += 1
+                    }
+                }
+            } else {
+                val deltaX = toX - fromX
+                val deltaY = toY - fromY
+                val maximum = kotlin.math.abs(deltaX).coerceAtLeast(kotlin.math.abs(deltaY))
+                for (i in 0..maximum) {
+                    val x = i/maximum.toDouble() * deltaX + fromX
+                    val y = i/maximum.toDouble() * deltaY + fromY
+                    grid[x.roundToInt()][y.roundToInt()] += 1
+                }
+            }
+        }
+        print(grid.joinToString("\n") { it.joinToString("")})
+        println("\n")
+        return countOccurrencesOverOne(grid)
     }
 
 
@@ -79,8 +126,8 @@ fun main() {
     val input = readInput("Day05")
     println(part1(inputTest))
     println(part1(input))
-    //println(part2(inputTest))
-    //println(part2(input))
+    println(part2(inputTest))
+    println(part2(input))
 
 }
 
