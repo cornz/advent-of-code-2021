f9dd264e691cd5b936dfbde435309a4ba7f53563
cornz <cornz@users.noreply.github.com>
2021-12-06 18:32:59 +0100

    shift shift shift, solve part2

--- src/Day06.kt
+++ src/Day06.kt
@@ -20,6 +20,25 @@ fun main() {
         }
     }
 
+    fun mapFish(input: List<String>): Map<Int, Long> = input[0]
+        .split(',')
+        .map { it.toInt() }
+        .groupingBy { it }
+        .eachCount()
+        .mapValues { (_, entry) -> entry.toLong() }
+
+    fun Map<Int, Long>.shiftUp() = mapOf(
+        0 to getOrDefault(1, 0),
+        1 to getOrDefault(2, 0),
+        2 to getOrDefault(3, 0),
+        3 to getOrDefault(4, 0),
+        4 to getOrDefault(5, 0),
+        5 to getOrDefault(6, 0),
+        6 to getOrDefault(7, 0) + getOrDefault(0, 0),
+        7 to getOrDefault(8, 0),
+        8 to getOrDefault(0, 0)
+    )
+
 
 
     fun part1(input: List<String>, days: Int): Long {
@@ -33,18 +52,24 @@ fun main() {
     }
 
     fun part2(input: List<String>, days: Int): Long {
-        return 0L
+        var fishes = mapFish(input)
+
+        repeat(256) {
+            fishes = fishes.shiftUp()
+        }
+
+        return fishes.values.sum()
     }
 
 
     val inputTest = readInput("Day06_test")
     val input = readInput("Day06")
-    println(part1(inputTest, 18))
-    println(part1(inputTest, 80))
-    println(part1(input, 80))
+    //println(part1(inputTest, 18))
+    //println(part1(inputTest, 80))
+    //println(part1(input, 80))
     //println(part1(input, 250))
-    //println(part2(inputTest, 256))
-    //println(part2(input, 256))
+    println(part2(inputTest, 256))
+    println(part2(input, 256))
 
 
 }
