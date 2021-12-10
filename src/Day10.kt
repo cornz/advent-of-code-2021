796990f6555218b97fae24f1f9a66bbcb10d2caf
cornz <cornz@users.noreply.github.com>
2021-12-10 14:59:41 +0100

    solve p02

--- src/Day10.kt
+++ src/Day10.kt
@@ -37,9 +37,46 @@ fun main() {
         return ret
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
+        val opening = listOf("[", "(", "{", "<")
+        val closing = listOf("]", ")", "}", ">")
+        val illegalPoints = hashMapOf(
+            ")" to 1,
+            "]" to 2,
+            "}" to 3,
+            ">" to 4
+        )
 
-        return 0
+
+        val lineValues = mutableListOf<Long>()
+
+        for (row: List<String> in input.map { it -> it.toCharArray().map { it.toString() } }) {
+            var ret = 0L
+            val openedChars = mutableListOf<String>()
+            var incompleteLine = true
+            for (char: String in row) {
+                if (opening.contains(char)) {
+                    openedChars.add(char)
+                } else {
+                    val correspondingOpeningChar = opening[closing.indexOf(char)]
+                    if (correspondingOpeningChar != openedChars.last()) {
+                        incompleteLine = false
+                        break
+                    } else {
+                        openedChars.removeLast()
+                    }
+                }
+            }
+            if (incompleteLine) {
+                val values = openedChars.reversed().map { closing[opening.indexOf(it)] }.map { illegalPoints[it]!! }
+                for (value: Int in values) {
+                    ret = ret * 5 + value
+                }
+                lineValues.add(ret)
+            }
+        }
+        println(lineValues)
+        return lineValues.sorted()[lineValues.size / 2]
     }
 
 
@@ -47,8 +84,8 @@ fun main() {
     val input = readInput("Day10")
     println(part1(inputTest))
     println(part1(input))
-    //println(part2(inputTest))
-    //println(part2(input))
+    println(part2(inputTest))
+    println(part2(input))
 
 }
 
